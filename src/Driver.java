d7c03c4ecd3d636c30e821f3357f6120f11bde5b
zlargon <zlargon1988@gmail.com>
2017-10-10 07:57:04 -0400

    implement class Student with constructor, getter, setter, and toString methods (15 Points)

--- src/Driver.java
+++ src/Driver.java
@@ -6,5 +6,8 @@ public class Driver {
 		// Test
 		Person person = new Person("Dan", "Peters", 16);
 		System.out.println(person);
+
+		Student student = new Student("Dan", "Peters", 16, 4.0);
+		System.out.println(student);
 	}
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,27 @@
 
 public class Student extends Person {
+	private double GPA;
 
+	// Constructor
+	public Student (final String firstName, final String lastName, final int age, final double gpa) {
+		super(firstName, lastName, age);
+		this.setGPA(gpa);
+	}
+
+	// GPA getter & setter
+	public double getGPA() {
+		return GPA;
+	}
+	public void setGPA(double gpa) {
+		if (gpa < 0 || gpa > 4) {
+			throw new RuntimeException("GPA (" + gpa + ") should range between 0.0 and 4.0");
+		}
+		GPA = gpa;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student " + super.toString() + ", GPA " + this.GPA;
+	}
 }
