f41481ed7ab273352b082df7b4cc939b5a8fb2a2
zlargon <zlargon1988@gmail.com>
2017-10-12 09:53:24 -0400

    implement School to generate student list and sort the list by GPA (10 Points)

--- src/Driver.java
+++ src/Driver.java
@@ -1,16 +1,8 @@
 
 public class Driver {
 
-	public static void main(String[] args) {
-
-		// Test
-		Person person = new Person("Dan", "Peters", 16);
-		System.out.println(person);
-
-		Student student = new Student("Dan", "Peters", 16, 4.0);
-		System.out.println(student);
-
-		Employee employee = new Employee("Dan", "Peters", 16, 15.75);
-		System.out.println(employee);
-	}
+    public static void main(String[] args) {
+        System.out.println("=================SCHOOL=================");
+        School.demo();
+    }
 }
---
+++ src/School.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class School {
+
+    private static List<Student> studentList = new ArrayList<Student>();
+
+    private static void generateList () {
+        Student student1 = (Student) Factory.createPerson(Factory.STUDENT, "Leon", "Huang", 28, 4.0);
+        Student student2 = (Student) Factory.createPerson(Factory.STUDENT, "Peter", "Huang", 22, 3.0);
+        Student student3 = (Student) Factory.createPerson(Factory.STUDENT, "Zoey", "Yung", 23, 3.5);
+        Student student4 = (Student) Factory.createPerson(Factory.STUDENT, "Michelle", "Cheng", 22, 3.2);
+
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        studentList.add(student4);
+    }
+
+    private static void showList () {
+        for (Student stu : studentList) {
+            System.out.println(stu);
+        }
+    }
+
+    private static void sortByGPA () {
+        Collections.sort(studentList, Student.CompareByGPA);
+    }
+
+    public static void demo () {
+        generateList();
+
+        System.out.println("BEFORE:");
+        showList();
+
+        sortByGPA();
+
+        System.out.println("\nAFTER:");
+        showList();
+    }
+}
