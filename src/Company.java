6b0d1442caad73dd6a6fe072d427d47c4dc8749f
zlargon <zlargon1988@gmail.com>
2017-10-12 09:57:31 -0400

    implement Company to generate employee list and sort the list by wage

---
+++ src/Company.java
@@ -0,0 +1,43 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Company {
+
+    private static List<Employee> employeeList = new ArrayList<Employee>();
+
+    private static void generateList () {
+        Employee employee1 = (Employee) Factory.createPerson(Factory.EMPLOYEE, "Leon", "Huang", 28, 15.0);
+        Employee employee2 = (Employee) Factory.createPerson(Factory.EMPLOYEE, "Peter", "Huang", 22, 25.0);
+        Employee employee3 = (Employee) Factory.createPerson(Factory.EMPLOYEE, "Zoey", "Yung", 23, 13.5);
+        Employee employee4 = (Employee) Factory.createPerson(Factory.EMPLOYEE, "Michelle", "Cheng", 22, 12.5);
+
+        employeeList.add(employee1);
+        employeeList.add(employee2);
+        employeeList.add(employee3);
+        employeeList.add(employee4);
+    }
+
+    private static void showList () {
+        for (Employee stu : employeeList) {
+            System.out.println(stu);
+        }
+    }
+
+    private static void sortByWage () {
+        Collections.sort(employeeList, Employee.CompareByWage);
+    }
+
+    public static void demo () {
+        generateList();
+
+        System.out.println("BEFORE:");
+        showList();
+
+        sortByWage();
+
+        System.out.println("\nAFTER:");
+        showList();
+    }
+}
--- src/Driver.java
+++ src/Driver.java
@@ -4,5 +4,8 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println("=================SCHOOL=================");
         School.demo();
+
+        System.out.println("\n=================COMPANY=================");
+        Company.demo();
     }
 }
